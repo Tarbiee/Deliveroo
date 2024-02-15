04a1200ff361849248dbf2f36647de96ff617d82
Tarbiee <dorcusstaby@gmail.com>
2024-02-16 01:25:46 +0300

    Create parcelOrder model

--- server/models.py
+++ server/models.py
@@ -24,3 +24,28 @@ class User(db.Model):
     @classmethod
     def get_user_by_username(cls, username):
         return cls.query.filter_by(username=username).first()
+    
+    # saving user
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    # deleting user
+    def delete_user(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class ParcelOrder(db.Model):
+    __tablename__ = 'parcel_orders'
+    id = db.Column(db.Integer, primary_key= True)
+    name_of_parcel = db.Column(db.String(50),unique=True, nullable=False)
+    pickup_location = db.Column(db.String(50))
+    destination = db.Column(db.String(50))
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    image_of_parcel = db.Column(db.String)
+    receivers_name = db.Column(db.String(50))
+    weight_of_parcel = db.Column(db.Integer)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    tracker = db.relationship('Tracker',uselist=False, backref='parcel_order')
