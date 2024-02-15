04e368e896d70163e22e4461f02526e4f6b3a15a
Tarbiee <dorcusstaby@gmail.com>
2024-02-16 01:30:18 +0300

    Add user registration endpoint

--- server/app.py
+++ server/app.py
@@ -24,6 +24,25 @@ auth_bp = Blueprint('auth', __name__)
 user_bp = Blueprint('users', __name__)
 admin_bp = Blueprint('admin', __name__)
 
+@auth_bp.post('/register')
+
+def register_user():
+    data = request.get_json()
+    
+    user = User.get_user_by_username(username= data.get('username'))
+
+    if user is not None:
+        return jsonify({"error": "User already exists"}), 403
+    
+    new_user = User(
+        username = data.get('username'),
+        email = data.get('email')
+    )
+    new_user.set_password(password= data.get('password'))
+
+    new_user.save_user()
+    return jsonify({"message":"User created"}), 201
+
 
 class Home(Resource):
     def get(self):
