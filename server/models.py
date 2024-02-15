eb56b9c60a30e5c0f6bd2d88f3b8d6cbc188c0ba
Tarbiee <dorcusstaby@gmail.com>
2024-02-16 01:13:50 +0300

    Create user model

---
+++ server/models.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.Text)
+
+    parcels = db.relationship('ParcelOrder', backref='user')
