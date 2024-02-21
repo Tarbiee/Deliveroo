a16eb718bfefb00bcc3b9007ce35359f75f48a9a
Tarbiee <dorcusstaby@gmail.com>
2024-02-21 10:06:50 +0300

    Login component

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import {useEffect, useState} from 'react';
+import {Route, Routes} from 'react-router-dom'
+import Login from './Login';
+import Register from './Register';
+import Home from './Home_page';
+import Admin from './Admin_page';
+import './App.css';
+
+function App() {
+  const [accessToken, setAccessToken] = useState(localStorage.getItem("accessToken"))
+
+  useEffect(() =>{
+    const storedAccessToken = localStorage.getItem("accessToken");
+    setAccessToken(storedAccessToken)
+  }, []);
+  
+  
+  console.log("This is:", accessToken)
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Login accessToken={accessToken}/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/home_page' element={<Home accessToken={accessToken}/>}/>
+        <Route path='/admin_page' element={<Admin accessToken={accessToken}/> }/>
+      </Routes>
+   
+    </div>
+  );
+}
+
+export default App;
