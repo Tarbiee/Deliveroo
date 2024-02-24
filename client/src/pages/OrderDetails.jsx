e9f32078dddde4c3987eb6eebe3f314ed1d7ed14
eric githaiga <ericgithaiga007@gmail.com>
2024-02-24 15:19:52 +0300

    status bar functionality

--- client/src/pages/OrderDetails.jsx
+++ client/src/pages/OrderDetails.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom'
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css"
@@ -8,6 +8,23 @@ import Footer from "../components/Footer";
 function OrderDetails({accessToken}) {
 
   const {id} = useParams()
+  const [currentStep, setCurrentStep] = useState(1)
+  const [complete, setComplete] = useState(false);
+  const  steps = ['preparing', 'on transit', 'delivered']
+  const position = [51.505, -0.09]  
+
+
+
+  useEffect(() => {
+    const status = "delivered"
+    const index = steps.indexOf(status);
+    if (index !== -1) {
+      setCurrentStep(index + 1);
+    } else if (index ===  "delivered") {
+      setComplete(true);
+    }
+  }, []); 
+
 
 
 
@@ -51,50 +68,32 @@ function OrderDetails({accessToken}) {
                
             </div>
             <div className="flex flex-wrap items-center  justify-center">
-                <div className="relative w-full px-4 mb-16 md:w-1/2 lg:w-1/4 lg:mb-0">
-                    <div className="absolute hidden top-5 lg:block left-1/2 ">
-                        <span
-                            className="mb-3 border-b-2 border-r border-blue-600 w-72 md:block left-1/2  inset-px">
-                        </span>
-                    </div>
-                    <div className="relative text-center">
-                        <span
-                            className="inline-flex items-center justify-center w-10 h-10 mb-8 text-lg text-gray-100 bg-blue-600 rounded-full shadow-md ">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                className="w-6 h-6 bi bi-check-lg" viewBox="0 0 16 16">
-                                <path
-                                    d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z" />
-                            </svg>
-                        </span>
-                        <h2 className="text-lg font-medium ">Preparing</h2>
-                    </div>
-                </div>
-                <div className="relative w-full px-4 mb-16 md:w-1/2 lg:w-1/4 lg:mb-0">
-                    <div className="absolute hidden top-5 lg:block left-1/2 ">
-                        <span
-                            className="mb-3 border-b-2 border-r border-gray-300 w-72 md:block left-1/2  inset-px">
-                        </span>
-                    </div>
-                    <div className="relative text-center">
-                        <span
-                            className="inline-flex items-center justify-center w-10 h-10 mb-8 text-lg text-gray-100 bg-blue-600 rounded-full shadow-md ">
-                            2
-                        </span>
-                        <h2 className="text-lg font-medium ">Shipping</h2>
-                    </div>
-                </div>
-                <div className="relative w-full px-4 mb-16 md:w-1/2 lg:w-1/4 md:mb-0">
-                    <div className="absolute hidden top-5 lg:block left-1/2 ">
-                       
-                    </div>
-                    <div className="relative text-center">
-                        <span
-                            className="inline-flex items-center justify-center w-10 h-10 mb-8 text-lg text-gray-700 bg-gray-200 rounded-full shadow-md ">
-                            3
-                        </span>
-                        <h2 className="text-lg font-medium ">Delivered</h2>
-                    </div>
-                </div>
+             
+                {steps?.map((step, i) => (
+                  <div key={i} className="relative w-full px-4 mb-16 md:w-1/2 lg:w-1/4 lg:mb-0">
+                  <div className="absolute hidden top-5 lg:block left-1/2 ">
+                      <span
+                          className={`mb-3 border-b-2 border-r border-gray-300  w-72 md:block left-1/2  inset-px ${currentStep === i + 1 && "border-blue-600"} ${
+                            (i + 1 < currentStep || complete )
+                          }`}>
+                      </span>
+                  </div>
+                  <div className="relative text-center">
+                      <span
+                          className={`inline-flex items-center justify-center w-10 h-10 mb-8 text-lg text-gray-100 bg-blue-600  ${currentStep === i + 1 && "bg-gray-300"} ${
+                            (i + 1 < currentStep || complete)
+                          } rounded-full shadow-md `}>
+                         { i + 1 < currentStep || complete  ?  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                              className="w-6 h-6 bi bi-check-lg" viewBox="0 0 16 16">
+                              <path
+                                  d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z" />
+                          </svg> : i + 1}
+                      </span>
+                      <h2 className="text-lg font-medium ">{step}</h2>
+                  </div>
+              </div>
+                ))}
+           
                
             </div>
         </div>
